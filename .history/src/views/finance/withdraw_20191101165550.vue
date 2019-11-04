4285f5d63122656cd261608397d41d5119e9453b
Songyu <sy15090195675@163.com>
2019-11-04 09:03:57 +0800

    增加页面

---
+++ .history/src/views/finance/withdraw_20191101165550.vue
@@ -0,0 +1,136 @@
+<template>
+  <!-- 清除缓存页面 -->
+  <div class="wscn-http404-container">
+    <div>
+      <div class="system-right">
+        <div class="right-main-box">
+          <div>
+            <div class="right-main right-main1" >
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  finan,
+  finana,
+  pages,
+  cashlist,
+  withdraw,
+  topup,
+  toup
+} from "@/api/finance"
+export default {
+
+  data() {
+    return {
+      ind:0,
+
+    }
+  },
+
+  methods:{
+    changeBgc: function (index) {
+      this.ind = index
+    },
+    onSubmit() {
+        // console.log('submit!');
+    },
+    onCopy() {
+      this.$message.success("复制成功！")
+    },
+    onError() {
+      this.$message.success("复制失败！")
+    },
+     handleClick(tab, event) {
+        console.log(tab, event);
+    },
+    // 折叠面板
+    handleChange(val) {
+      console.log(val);
+    }
+  },
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.wscn-http404-container {
+  background: #f0f2f5;
+  min-height: calc(100vh - 84px);
+  width: 100%;
+  background-color: #f0f2f5;
+  background-size: cover;
+}
+.wscn-http404-container>div{
+  width:100%;
+  min-width:650px;
+}
+// 系统设置的主体右边主体内容
+.system-right{
+  width: 100%;
+  min-width: 500px;
+  float: right;
+  min-height: 100%;
+  box-sizing: border-box;
+  padding:0px;
+  margin:0px;
+}
+.right-main-box{
+  padding:15px;
+  width:100%;
+  box-sizing: border-box;
+}
+.right-main{
+  width:100%;
+  box-sizing: border-box;
+  padding:20px;
+  background: #fff;
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+}
+.left-Line{
+  height: 24px;
+  line-height: 24px;
+  padding-left: 5px;
+  border-left: 2px solid #409eff;
+  margin-bottom: 20px;
+  font: 14px Helvetica Neue,Helvetica,PingFang SC,Tahoma,Arial,sans-serif;
+  color: #303133;
+}
+.kaifang-weixin{
+  width:200px;
+  line-height: 24px;
+  color: #303133;
+  font: 14px Helvetica Neue,Helvetica,PingFang SC,Tahoma,Arial,sans-serif;
+}
+.clickspan{
+  display:inline-block;
+  margin-left:20px;
+  cursor:pointer;
+  color: #999
+}
+.clickspan:hover{
+  color: #409EFF;
+}
+
+</style>
+<style>
+  .system-right .el-checkbox {
+    padding: 9px 20px 9px 10px;
+    border-radius: 4px;
+    border: 1px solid #DCDFE6;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    line-height: normal;
+    height: 36px;
+    margin: 5px;
+  }
+  .system-right .el-checkbox.is-bordered.is-checked {
+    border-color: #409EFF;
+}
+</style>
